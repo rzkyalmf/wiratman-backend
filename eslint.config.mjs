42585853aa27cf75a84ef4124463e12c972b5ff7
Rizky Alam <sketsacartoon@gmail.com>
2024-10-19 14:59:50 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
