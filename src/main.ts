7da853b40c6965281433f61a3ee8a26239059616
Rizky Alam <sketsacartoon@gmail.com>
2024-10-19 14:31:27 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+const data = {
+  name: "wiratman",
+};
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
